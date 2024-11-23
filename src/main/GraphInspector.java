43566b0a2f0e216b4e5c2beb3a4d76484994f720
Shane Ruegg <160198521+SPRuegg@users.noreply.github.com>
2024-11-23 10:54:52 -0600

    Create GraphInspector.java

---
+++ src/main/GraphInspector.java
@@ -0,0 +1,17 @@
+package main;
+
+/**
+ * Provides tools for analyzing and extracting information from a graph.
+ * Users can interact through a text-based interface to select specific
+ * options for inspecting various aspects of the graph.
+ *
+ * @author Shane Ruegg
+ * @since 11-23-2024
+ * @version 1.0.0
+ */
+
+public class GraphInspector {
+    public static void main(String[] args) {
+
+    }
+}
