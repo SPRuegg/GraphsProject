51904fc2e969e5f058d386b1525813f850e9c509
Shane Ruegg <160198521+SPRuegg@users.noreply.github.com>
2024-11-24 15:00:13 -0600

    Update Graph.java
    Added, helper method. Implemented contains() method, and insert() method.

--- src/graphs/Graph.java
+++ src/graphs/Graph.java
@@ -199,6 +199,26 @@ public class Graph <E extends Comparable<E>> {
     }
 
     /**
+     * Auxiliary method to retrieve a vertex from a key, if it
+     * exists.
+     *
+     * @param data the key/data of the vertex to be located
+     * @return the vertex, if it exists, or null otherwise
+     */
+    private Vertex retrieveVertex(E data) {
+        Vertex curr = first;
+        while (curr != null && cmp.compare(data, curr.data) > 0) {
+            curr = curr.nextVertex;
+        }
+
+        if (curr == null || cmp.compare(data, curr.data) != 0) {
+            return null;
+        }
+
+        return curr;
+    }
+
+    /**
      * Checks if the graph contains a vertex with the specified data.
      *
      * @param data the data to search for in the graph
@@ -206,9 +226,11 @@ public class Graph <E extends Comparable<E>> {
      *         <b>false</b> otherwise
      */
     public boolean contains(E data) {
-        // To be implemented
+        if (isEmpty()) return false;
 
-        return false;
+        Vertex vert = retrieveVertex(data);
+
+        return vert != null;
     }
 
     /**
@@ -220,7 +242,41 @@ public class Graph <E extends Comparable<E>> {
      *         <b>false</b> if the data already exists in the graph
      */
     public boolean insertVertex(E data) {
-        // To be implemented
+        if (isEmpty()) {
+            first = new Vertex();
+            first.data = data;
+            return true;
+        }
+
+        // Special Case: first vertex
+        if (cmp.compare(data, first.data) < 0) {
+            Vertex newVert = new Vertex();
+            newVert.data = data;
+
+            // LinkedList head reassignment
+            newVert.nextVertex = first;
+            first = newVert;
+        } else if (cmp.compare(data, first.data) == 0) {
+            return false;
+        }
+
+        Vertex curr = first;
+
+        // Find correct position
+        while (curr.nextVertex != null && cmp.compare(data, curr.nextVertex.data) > 0) {
+            curr = curr.nextVertex;
+        }
+
+        // Ensure the requested data, doesn't exist already
+        if (cmp.compare(data, curr.data) != 0) {
+            Vertex newVert = new Vertex();
+            newVert.data = data;
+
+            // LinkedList insertion
+            newVert.nextVertex = curr.nextVertex;
+            curr.nextVertex = newVert;
+            return true;
+        }
 
         return false;
     }
