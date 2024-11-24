9902c93c6cf5521ae9cbf6ea1feaadede589566d
Shane Ruegg <160198521+SPRuegg@users.noreply.github.com>
2024-11-23 22:56:54 -0600

    Create GraphException.java

---
+++ src/graphs/GraphException.java
@@ -0,0 +1,10 @@
+package graphs;
+
+public class GraphException extends Exception {
+
+    public GraphException() {}
+
+    public GraphException(String message) {
+        super(message);
+    }
+}
